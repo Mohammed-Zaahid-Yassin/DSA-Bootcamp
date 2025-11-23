48325b462f5f5641db1782dfe07c1521e6c513a9
Mohammed-Zaahid-Yassin <mohammedzaahidyassin21@gmail.com>
2025-11-23 18:33:58 +0530

    Initial commit

---
+++ Pallindrome_checker.py
@@ -0,0 +1,7 @@
+#Pallindrome_Checker
+def is_palindrome(s):
+    # Remove spaces and convert to lowercase
+    s = s.replace(" ", "").lower()
+    # Check if the string is equal to its reverse
+    return s == s[::-1]
+
