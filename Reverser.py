8d088427aaeb8b39531e8be5bf4f6a242d59badd
Mohammed-Zaahid-Yassin <mohammedzaahidyassin21@gmail.com>
2025-11-25 20:17:21 +0530

    Update-1

---
+++ Reverser.py
@@ -0,0 +1,9 @@
+#Reverse an array or a string
+def reverse_array(arr):
+    start = 0
+    end = len(arr) - 1
+    while start < end:
+        arr[start], arr[end] = arr[end], arr[start]
+        start += 1
+        end -= 1
+    return arr 
